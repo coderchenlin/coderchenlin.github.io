863cc124a5197f7763cd7cbaaf3cb9cf6884891b
coderchenlin <chenlin048830@gmail.com>
2022-11-13 01:49:05 +0800

    backup

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+echo "Start"
+git add .
+git commit -m backup
+git checkout br-back
+git cherry-pick master
+git push origin br-back
+git checkout master
+hexo g
+hexo d
+echo "Finish"
+echo 按任意键继续
+read -n 1
